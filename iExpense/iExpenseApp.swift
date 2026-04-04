feaf343f47f64582d95a0817c5f7a9aec2c3a68e
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-04 10:33:38 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Brayan Duwe on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
