a3f58ac58c8fb4bfc05b9b494ecd74ae6be7bae6
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-07 21:25:59 -0300

    iExp. Identifiable items

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -16,11 +17,11 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
-            }
+            } 
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add expense", systemImage: "plus") {
