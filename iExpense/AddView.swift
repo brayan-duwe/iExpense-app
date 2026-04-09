47e5d62c4b3a4b85a928010fc98e92a797635cca
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-09 19:13:06 -0300

    iExp. showing Amount and Type on the list

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -28,6 +30,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
